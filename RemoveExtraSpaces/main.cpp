f416795933616046d093d2943521f47a8f252750
Ata Gowani <atagowani@gmail.com>
2017-02-09 11:26:33 -0600

    Initial Commit

---
+++ RemoveExtraSpaces/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RemoveExtraSpaces
+//
+//  Created by Ata Gowani on 2/9/17.
+//  Copyright © 2017 Ata Gowani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
